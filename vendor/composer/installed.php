86d258b7a943d01e357d2261bbe728fe9aab91b5
yoannclaisse <claisse.yoann@gmail.com>
2022-04-21 20:16:03 +0200

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8ef1abe910dc2b765b025ebc2b7ce8d2726a0786',
+        'name' => 'project/comics-shop',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'project/comics-shop' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8ef1abe910dc2b765b025ebc2b7ce8d2726a0786',
+            'dev_requirement' => false,
+        ),
+    ),
+);
